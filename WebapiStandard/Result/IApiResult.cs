80842e786cd2e03fd91a51bce1accbfb0f4802ee
Fawei <50777393@qq.com>
2025-07-12 16:13:39 +0800

    webapi - api result

---
+++ WebapiStandard/Result/IApiResult.cs
@@ -0,0 +1,18 @@
+﻿namespace WebapiStandard.Result
+{
+    public interface IApiResult<T> where T : class
+    {
+        /// <summary>
+        ///     Gets or sets the data.
+        /// </summary>
+        T? Data { get; set; }
+        /// <summary>
+        ///     Gets or sets the message.
+        /// </summary>
+        string? Message { get; set; }
+        /// <summary>
+        ///     Gets or sets the status code.
+        /// </summary>
+        int StatusCode { get; set; }
+    }
+}
