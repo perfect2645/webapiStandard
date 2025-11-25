16fe87224b247707349c5943ccd15f59190577e0
Fawei <50777393@qq.com>
2025-11-25 19:58:59 +0800

    webapi - add shirt repository

---
+++ Fawei.Repository/shirt/ShirtRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fawei.Repository.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fawei.Repository.shirt
+{
+    public class ShirtRepository : RepositoryBase<Shirt>
+    {
+        public ShirtRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
