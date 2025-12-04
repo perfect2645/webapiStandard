16281039ab8472087b2b1ee9512aee95b0224f20
Fawei <50777393@qq.com>
2025-12-04 22:09:36 +0800

    webapi - shirt controller

--- WebapiNet10/Controllers/Shirts/ShirtController.cs
+++ WebapiNet10/Controllers/Shirts/ShirtController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebapiNet10.Models.Shirts;
 using WebapiNet10.Services.Shirts;
 
 namespace WebapiNet10.Controllers.Shirt
@@ -9,17 +10,62 @@ namespace WebapiNet10.Controllers.Shirt
     {
         private readonly ILogger<ShirtController> _logger;
         private readonly IShirtService _shirtService;
+
+        /// <summary>
+        /// Shirt Controller
+        /// </summary>
+        /// <param name="loggerFactory"></param>
+        /// <param name="shirtService"></param>
         public ShirtController(ILoggerFactory loggerFactory, IShirtService shirtService)
         {
             _logger = loggerFactory.CreateLogger<ShirtController>();
             _shirtService = shirtService;
         }
 
-        [HttpGet("shirts")]
-        public IActionResult GetAllShirts()
+        /// <summary>
+        /// Get all shirts
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAllShirtsAsync()
         {
-            var shirtDtos = _shirtService.GetAllShirtsAsync();
+            var shirtDtos = await _shirtService.GetAllShirtsAsync();
             return Ok(shirtDtos);
+        }
+
+        /// <summary>
+        /// Get by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetShirtByIdAsync(int id)
+        {
+            var shirtDto = HttpContext.Items.TryGetValue("ShirtDto", out var dto) ? dto as ShirtDto : null;
+            return Ok(shirtDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateShirtAsync([FromBody] CreateShirtDto shirtDto)
+        {
+            var createdShirtDto = await _shirtService.AddShirtAsync(shirtDto);
+            return CreatedAtAction(nameof(GetShirtByIdAsync),
+                new { id = createdShirtDto.ShirtId },
+                createdShirtDto);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateShirtAsync(int id, [FromBody] ShirtDto shirtDto)
+        {
+            await _shirtService.UpdateShirtAsync(shirtDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteShirtAsync(int id)
+        {
+            var shirtDto = await _shirtService.DeleteShirtAsync(id);
+            return Ok(shirtDto);
         }
     }
 }
