315cc705776999a5e352e576b0cc6dc338f07148
Fawei <50777393@qq.com>
2025-12-04 22:22:12 +0800

    utils - add dbcontext register extension

---
+++ Fawei.Repository.Core/Configurations/DbContextRegister.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fawei.Repository.Core.Configurations
+{
+    public static class DbContextRegister
+    {
+        public static void AddSqlServerContext<TContext>(this WebApplicationBuilder builder, string configPath) where TContext : DbContext
+        {
+            builder.Services.AddDbContext<TContext>(options =>
+            {
+                //Data Source=DESKTOP-XICHENG\FAWEISQLEXPRESS;Initial Catalog=master;Integrated Security=True;Trust Server Certificate=True
+                options.UseSqlServer(builder.Configuration.GetConnectionString(configPath));
+            });
+        }
+    }
+}
--- WebapiNet10/Program.cs
+++ WebapiNet10/Program.cs
@@ -3,12 +3,9 @@ using WebapiNet10.Configurations.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 builder.Logging.AddLog4Net("log4net.config");
+builder.AddDbContext();
 
-Log4Logger.Logger.Debug("debug1");
-Log4Logger.Logger.Warn("warn3");
 // Add services to the container.
 builder.RegisterServices();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
